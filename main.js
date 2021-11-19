46d6462796a1f8a8798890b6fc57fa57d6847482
Saisharvesh07 <72559801+Saisharvesh07@users.noreply.github.com>
2021-11-19 11:21:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+noseX=0;
+noseY=0;
+leftWrist=0;
+rightWrist=0;
+difference=0;
+
+function setup(){
+video = createCapture(VIDEO);
+video.size(560, 500);
+
+canvas = createCanvas(550, 550);
+canvas.position(560,150);
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+console.log('PoseNet is Initialized');
+}
+
+function draw(){
+background('#0ee3ce')
+}
+
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+console.log(results);
+}
+}
